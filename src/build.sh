de2a7d46b61c4b089ce68dd1a1b3075b258f235d
rynsf <rynsf@users.noreply.github.com>
2026-04-12 18:09:46 +0530

    update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
  #!/bin/sh
 
 #gcc main.c -o sagan_test.out -O3 -fsanitize=address -g -lm && ./sagan_test.out
-gcc main.c -o sagan_test.out -O3 -lm && ./sagan_test.out
+gcc main.c -o sagan.out -O3 -lm
